c628db42e5b0aad07140f7bdb85e62d9ab27b69f
Ethan Shepherd <ethan.m.shepherd@gmail.com>
2021-10-13 20:08:00 -0700

    working on server and mongodb cache

--- server/server.js
+++ server/server.js
@@ -1,19 +1,117 @@
 const express = require("express");
+const fetch = require("node-fetch");
+const secret = require("./secret.json");
+var MongoClient = require("mongodb").MongoClient;
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+let bearerToken, expiresDate;
+
+const API_BASE = "https://api.spotify.com/v1";
+
+const AUTH_BASE = "https://accounts.spotify.com/api/token";
+const CLIENT_ID = "fd89d09a42cf4a9fb0a69ec19e3432ae";
+const SECRET_ID = secret.key;
+const ENCODED_ID = Buffer.from(CLIENT_ID + ":" + SECRET_ID).toString("base64");
+
+function setNewExpires(expires) {
+  const currentDateObj = new Date();
+  const numberOfMlSeconds = currentDateObj.getTime();
+  const addMlSeconds = expires * 1000;
+  const newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
+  expiresDate = newDateObj;
+}
+
+function tokenIsExpired() {
+  if (!expiresDate || new Date().getTime() > expiresDate.getTime()) {
+    return true;
+  }
+}
+
+async function requestBearerToken() {
+  const response = await fetch(AUTH_BASE, {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      Authorization: "Basic " + ENCODED_ID,
+      "Content-Type": "application/x-www-form-urlencoded",
+    },
+    body: "grant_type=client_credentials",
+  });
+  return response.json();
+}
+
+async function getBearerToken() {
+  if (bearerToken === null || tokenIsExpired()) {
+    const json = await requestBearerToken();
+    const token = json["access_token"];
+    const expires = json["expires_in"];
+    bearerToken = token;
+    setNewExpires(expires);
+    return token;
+  } else {
+    return bearerToken;
+  }
+}
+
 // app.get("/api/search/:search", (req, res) => {
 //   res.json({ message: "Hello from the API" });
 // });
 
-app.get("/api/bands/:search", (req, res) => {
-  res.json({ message: `bands api endpoint: ${req.params.search}` });
+app.get("/api/bands/:search", async (req, res) => {
+  const token = await getBearerToken();
+  const searchUrl =
+    API_BASE + `/search?q=${req.params.search}&type=artist&limit=10`;
+  const bandInfoRequest = await fetch(searchUrl, {
+    method: "GET",
+    headers: {
+      Authorization: "Bearer " + token,
+    },
+  });
+  const bandInfo = await bandInfoRequest.json();
+  if (bandInfo.artists && bandInfo.artists.items) {
+    // setBands(bandInfo.artists.items);
+    res.json({ bands: bandInfo.artists.items });
+  } else {
+    // setBands([]);
+    res.json({ bands: [] });
+  }
 });
 
-app.get("/api/albums/:band", (req, res) => {
-  res.json({ message: "albums api endpoint" });
+app.get("/api/albums/:band", async (req, res) => {
+  MongoClient.connect(
+    "mongodb://localhost:27017/setlistr",
+    async function (err, client) {
+      if (err) throw err;
+
+      var db = client.db("albums");
+
+      db.collection("mammals")
+        .find()
+        .toArray(function (err, result) {
+          if (err) throw err;
+
+          console.log(result);
+        });
+      // TODO: check db
+      // TODO: pagination
+      // TODO: write to db
+      let albumInfo;
+      const token = await getBearerToken();
+      const searchUrl = API_BASE + `/artists/${req.params.band}/albums`;
+      const albumInfoRequest = await fetch(searchUrl, {
+        method: "GET",
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      });
+      const albumRes = await albumInfoRequest.json();
+      console.log(albumRes);
+      res.json({ albums: albumInfo });
+    }
+  );
 });
 
 // app.get("/api/tracks/", (req, res) => {});
--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,117 +1,46 @@
 import { useState, useEffect } from "react";
-import secret from "./secret.json";
+// import secret from "./secret.json";
 import BandDataList from "./BandDataList.js";
 
 const SearchParams = () => {
-  const [bearerToken, setBearerToken] = useState(null);
-  const [expiresDate, setExpiresDate] = useState(null);
   const [bandInput, setBandInput] = useState("");
   const [bands, setBands] = useState([]);
   const [bandName, setBandName] = useState("");
   const [bandId, setBandId] = useState("");
-
-  const CLIENT_ID = "fd89d09a42cf4a9fb0a69ec19e3432ae";
-  const SECRET_ID = secret.key;
-  const ENCODED_ID = Buffer.from(CLIENT_ID + ":" + SECRET_ID).toString(
-    "base64"
-  );
-
-  // const AUTH_BASE = "https://accounts.spotify.com/api/token";
-  const API_BASE = "https://api.spotify.com/v1";
-
-  function setNewExpires(expires) {
-    const currentDateObj = new Date();
-    const numberOfMlSeconds = currentDateObj.getTime();
-    const addMlSeconds = expires * 1000;
-    const newDateObj = new Date(numberOfMlSeconds + addMlSeconds);
-    setExpiresDate(newDateObj);
-  }
-
-  function tokenIsExpired() {
-    if (expiresDate === null || new Date().getTime() > expiresDate.getTime()) {
-      return true;
-    }
-  }
-
-  // TODO: this will need to be server-side, and return the bearer token
-  async function requestBearerToken() {
-    const response = await fetch(AUTH_BASE, {
-      method: "POST",
-      mode: "cors",
-      headers: {
-        Authorization: "Basic " + ENCODED_ID,
-        "Content-Type": "application/x-www-form-urlencoded",
-      },
-      body: "grant_type=client_credentials",
-    });
-    return response.json();
-  }
-
-  async function getBearerToken() {
-    if (bearerToken === null || tokenIsExpired()) {
-      const json = await requestBearerToken();
-      const token = json["access_token"];
-      const expires = json["expires_in"];
-      setBearerToken(token);
-      setNewExpires(expires);
-      return token;
-    } else {
-      return bearerToken;
-    }
-  }
+  const [albums, setAlbums] = useState({});
 
   //TODO: add year constraints option
 
-  async function getBandInfo() {
+  async function getBands() {
     const bandInfoRequest = await fetch(`/api/bands/${bandInput}`, {
       method: "GET",
     });
-    const bandInfo = await bandInfoRequest.json();
-    console.log(bandInfo);
-
-    // const token = await getBearerToken();
-    // const searchUrl = API_BASE + `/search?q=${bandInput}&type=artist&limit=10`;
-    // const bandInfoRequest = await fetch(searchUrl, {
-    //   method: "GET",
-    //   headers: {
-    //     Authorization: "Bearer " + token,
-    //   },
-    // });
-    // const bandInfo = await bandInfoRequest.json();
-    // if (bandInfo.artists && bandInfo.artists.items) {
-    //   setBands(bandInfo.artists.items);
-    // } else {
-    //   setBands([]);
-    // }
+    const res = await bandInfoRequest.json();
+    setBands(res.bands);
   }
 
-  // useEffect(() => {
-  //   console.log("useEffect: " + bandId);
-  //   if (!bandId) return null;
-  //   getAlbumInfo();
-  // }, [bandId]);
+  useEffect(() => {
+    console.log("useEffect: " + bandId);
+    if (!bandId) return null;
+    getAlbumInfo();
+  }, [bandId]);
 
-  // async function getAlbumInfo() {
-  //   console.log("getAlbumInfo");
-  //   const token = await getBearerToken();
-  //   const searchUrl = API_BASE + `/artists/${bandId}/albums`;
-  //   const albumInfoRequest = await fetch(searchUrl, {
-  //     method: "GET",
-  //     headers: {
-  //       Authorization: "Bearer " + token,
-  //     },
-  //   });
-  //   const albumInfo = await albumInfoRequest.json();
-  //   console.log(albumInfo);
-  //   // if(albumdInfo)
-  // }
+  async function getAlbumInfo() {
+    console.log("getAlbumInfo");
+    const albumInfoRequest = await fetch(`/api/albums/${bandId}`, {
+      method: "GET",
+    });
+    const res = await albumInfoRequest.json();
+    setAlbums(res.albums);
+    // setTracks(res.tracks);
+  }
 
   return (
     <div>
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          getBandInfo();
+          getBands();
         }}
       >
         <label htmlFor="band-input">
